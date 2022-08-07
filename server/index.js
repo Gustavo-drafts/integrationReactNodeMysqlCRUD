3c188c36b7d8553348eaffd4edff9b5fdc2db4e3
gutoo01 <gu.trindadelive@gmail.com>
2022-08-07 19:07:17 -0300

    integrationInCourse

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+/** Conectando ao banco de dados */
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user:"root",
+  password:"",
+  database:"crud",
+});
+/** --------------------------- */
+
+app.use(cors());
+app.use(express.json())
+
+app.post('/register', (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let SQL = "INSERT INTO products ( name, cost, category ) VALUES (?,?,?)";
+
+  db.query(SQL, [name, cost, category], (err, result) => {
+    console.log(err);
+  })
+})
+
+
+app.listen(3001, () => {console.log("Run!")})
